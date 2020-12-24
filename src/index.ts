543f3e2121438af7b1f9525a309fccb5c4d06d26
Eli Simkin <es340716@g.risd.org>
2020-12-24 14:24:19 -0600

    Added a project init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import fs from 'fs';
 import { join } from "path";
 
 export const __PROJ_NAME = join(__dirname, 'protected');
 
 export function main() {
-    
+    createProjectFolderIfNonexistant();
+}
+
+function createProjectFolderIfNonexistant() {
+    if (!fs.existsSync(__PROJ_NAME))
+        fs.mkdirSync(__PROJ_NAME);
 }
