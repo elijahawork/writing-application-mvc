8d864e82e1ae6a8d2c12da3966c045dbb9ce83ff
Eli Simkin <es340716@g.risd.org>
2020-12-24 12:20:41 -0600

    Changed MenuView so that it is singular

--- src/view/menu/FileMenuView.ts
+++
@@ -1,7 +0,0 @@
-import { MenuView } from "./MenuView";
-
-export class FileMenuView extends MenuView {
-    constructor(label?: string) {
-        super(label);
-    }
-}
--- src/view/menu/MenuView.ts
+++ src/view/menu/MenuView.ts
@@ -1,9 +1,14 @@
 import { CustomElement } from "../CustomElement";
-import { FolderMenuView } from "./FolderMenuView";
 
-export abstract class MenuView extends CustomElement<'li'> {
+const menuViewInstances: MenuView[] = [];
+
+export class MenuView extends CustomElement<'li'> {
     public readonly labelElement: HTMLButtonElement = document.createElement('button');
-    public parent: FolderMenuView | null = null;
+    private readonly listElement: HTMLUListElement = document.createElement('ul');
+
+    public parent: MenuView | null = null;
+    private children: MenuView[] = [];
+
     constructor(label?: string) {
         super('li');
 
@@ -17,5 +22,27 @@ export abstract class MenuView extends CustomElement<'li'> {
     }
     public get label() {
         return this.labelElement.textContent!;
+    }
+
+    public push(menuView: MenuView) {
+        this.listElement.appendChild(menuView.htmlElement);
+    }
+    
+    public insert(menuView: MenuView, index: number) {
+        this.listElement.children[index].insertAdjacentElement('beforebegin', menuView.htmlElement);
+    }
+
+    public get(index: number): MenuView {
+        return this.listElement;
+    }
+    public set(index: number, menuView: MenuView) {
+        this.remove(this.get(index));
+        this.insert(menuView, index - 1);
+    }
+
+
+    
+    public remove(menuView: MenuView) {
+        menuView.htmlElement.remove();
     }
 }
