bdfe33ab136474b4c0e3951897097673f3d64a96
unary-code <60202695+unary-code@users.noreply.github.com>
2020-12-27 17:06:15 -0600

    Create TextArea.ts
    TextArea

---
+++ src/view/text-area/TextArea.ts
@@ -0,0 +1,17 @@
+import { CustomElement } from "../CustomElement";
+
+//for now we only have one TextArea object
+export class TextArea {
+    x: number;
+
+    constructor(x: number) {
+        this.x = x;
+    }
+
+    public getX() {
+        return this.x;
+    }
+}
+
+var text: TextArea = new TextArea(9);
+console.log("x=" + text.getX());
