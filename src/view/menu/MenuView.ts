d2d77235e08e6213f58c5325f1cededa2a530afe
Elijah Simkin <elijah.a.work@gmail.com>
2020-12-28 00:30:13 -0600

    Fixed MenuView not lining up with List set() get() functions

--- src/view/menu/MenuView.ts
+++ src/view/menu/MenuView.ts
@@ -15,6 +15,13 @@ export class MenuView extends CustomElement<'li'> implements List<MenuView> {
         return this.labelElement.textContent!;
     }
 
+    public get(index: number): MenuView {
+        return this.arrayList.get(index);
+    }
+    public set(index: number, value: MenuView): void {
+        this.arrayList.set(index, value);
+    }
+
     constructor(label?: string) {
         super('li');
 
@@ -26,16 +33,16 @@ export class MenuView extends CustomElement<'li'> implements List<MenuView> {
         this.memoizeInstance();
     }
 
-    add(view: MenuView): void;
-    add(view: MenuView, index: number): void;
-    add(view: MenuView, index?: number): void {
+    public add(view: MenuView): void;
+    public add(view: MenuView, index: number): void;
+    public add(view: MenuView, index?: number): void {
         // don't fight with the type system. overloading is working weirdly
         this.arrayList.add(view, index!); 
     }
 
-    remove(view: MenuView): void;
-    remove(index: number): void;
-    remove(predicate: number | MenuView): void {
+    public remove(view: MenuView): void;
+    public remove(index: number): void;
+    public remove(predicate: number | MenuView): void {
         // don't fight with the type system on this. overloading is working weirdly
         this.arrayList.remove(predicate as number);
     }
