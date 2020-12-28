4a1e44a16cc817ae02d12a21e5e4666e40078429
Elijah Simkin <elijah.a.work@gmail.com>
2020-12-27 23:17:54 -0600

    Added a function to be called where testing code should be inputed

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import fs from 'fs';
 import { join } from "path";
-import { TextArea } from './view/text-area/TextArea';
-//import "src/view/text-area/TextArea";
-//import TextArea from './view/text-area/TextArea';
 
 export const __PROJ_NAME = join(__dirname, 'protected');
 
 export function main() {
     createProjectFolderIfNonexistant();
-    console.log("TESTING. PRINTED FROM WITHIN INDEX.TS");
-    let test: TextArea = new TextArea(8);
-    //const curX = test.getX();
-    //console.log("test.x= " + curX);
+    test();
 }
 
 function createProjectFolderIfNonexistant() {
     if (!fs.existsSync(__PROJ_NAME))
         fs.mkdirSync(__PROJ_NAME);
+}
+
+// run all testing code in here.
+function test() {
+
 }
