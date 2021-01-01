4ad31ce540d59967164542db245894341399b560
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-01 02:03:23 -0600

    House keeping

--- src/view/CustomElement.ts
+++ src/view/CustomElement.ts
@@ -4,7 +4,7 @@ export abstract class CustomElement<T extends keyof HTMLElementTagNameMap> {
     public readonly htmlElement: HTMLElementTagNameMap[T];
 
     public readonly tagName: string;
-    //public clientRect: ClientRect;
+    
     constructor(tagName: T, className?: string) {
         this.tagName = tagName;
         this.htmlElement = document.createElement(tagName);
@@ -13,9 +13,9 @@ export abstract class CustomElement<T extends keyof HTMLElementTagNameMap> {
             this.htmlElement.classList.add(className);
     }
 
-    public get pos():Coordinate {
-        //console.log("get pos()");
+    public get pos(): Coordinate {
         const boundingClientRect = this.htmlElement.getBoundingClientRect();
+
         if (boundingClientRect)
             return { x: boundingClientRect.x, y: boundingClientRect.y };
         
