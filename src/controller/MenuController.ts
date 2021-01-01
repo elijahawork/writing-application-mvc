8e1602ce76f5c18c383523b559dfbc153e38a987
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-01 02:00:33 -0600

    Cleaned up code. Removed outdated dependencies

--- src/controller/MenuController.ts
+++ src/controller/MenuController.ts
@@ -1,9 +1,8 @@
 import { DataModel } from "../model/DataModel";
+import { Coordinate } from "../types/Coordinate";
 import { MenuView } from "../view/menu/MenuView";
 
 const allMenuControllers: MenuController[] = [];
-
-type Coordinate = { x: number, y: number };
 
 function getNearestMenuController({ x, y }: Coordinate, ignore?: MenuController): MenuController {
     let minController: MenuController | null = null;
@@ -41,44 +40,42 @@ export class MenuController {
     public readonly menuView: MenuView;
     public readonly dataModel: DataModel;
 
+    public static from(id: number, position: number, label: string) {
+        return new MenuController(new MenuView(label), new DataModel(id, position, label));
+    }
+
     constructor(menuView: MenuView, dataModel: DataModel) {
-        allMenuControllers.push(this);
         this.menuView = menuView;
         this.dataModel = dataModel;
 
+        this.addMouseDownDraggingEvent();
+        this.addDragEndEvent();
+        this.addDragEvent();
+
+        this.memoize();
+    }
+    
+    private addMouseDownDraggingEvent() {
         this.menuView.labelElement.addEventListener('mousedown', (ev) => {
             ev.stopPropagation();
             this.menuView.htmlElement.draggable = true;
         });
+    }
+    private addDragEndEvent() {
         this.menuView.htmlElement.addEventListener('dragend', (ev) => {
             ev.stopPropagation();
             this.menuView.htmlElement.draggable = false;
             const { clientX: x, clientY: y } = ev;
             const nearestController = getNearestMenuController({ x, y }, this);
-            nearestController.push(this);
-
         })
+    }
+    private addDragEvent() {
         this.menuView.htmlElement.addEventListener('drag', (ev) => {
             ev.stopPropagation();
         });
     }
 
-    
-    public static from(id: number, position: number, label: string) {
-        return new MenuController(new MenuView(label), new DataModel(id, position, label));
-    }
-    public push(menuController: MenuController) {
-        console.log('push');
-        
-        this.menuView.push(menuController.menuView);
-        this.dataModel.push(menuController.dataModel);
-    }
-    public insert(menuController: MenuController, index: number) {
-        this.menuView.insert(menuController.menuView, index);
-        this.dataModel.insert(menuController.dataModel, index);
-    }
-    public remove(menuController: MenuController) {
-        this.menuView.remove(menuController.menuView);
-        this.dataModel.remove(menuController.dataModel);
+    private memoize() {
+        allMenuControllers.push(this);
     }
 }
