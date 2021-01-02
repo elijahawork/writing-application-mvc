c8cf3d348c1ad5d6712478cb92358f970358c4e2
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-01 18:04:16 -0600

    MenuController now implements List<MenuController>

--- src/controller/MenuController.ts
+++ src/controller/MenuController.ts
@@ -1,3 +1,5 @@
+import { List } from "../interfaces/List";
+import { ArrayList } from "../lib/ArrayList";
 import { DataModel } from "../model/DataModel";
 import { Coordinate } from "../types/Coordinate";
 import { MenuView } from "../view/menu/MenuView";
@@ -36,9 +38,10 @@ function getCoordinatesOfElementCenter(el: HTMLElement): Coordinate {
     return { x: box.left + box.width / 2, y: box.top + box.height / 2 };
 }
 
-export class MenuController {
+export class MenuController implements List<MenuController> {
     public readonly menuView: MenuView;
     public readonly dataModel: DataModel;
+    private controllers: ArrayList<MenuController> = new ArrayList<MenuController>();
 
     public static from(id: number, position: number, label: string) {
         return new MenuController(new MenuView(label), new DataModel(id, position, label));
@@ -53,6 +56,39 @@ export class MenuController {
         this.addDragEvent();
 
         this.memoize();
+    }
+
+    public set(index: number, value: MenuController) {
+        throw new Error(`Cannot replace a menu view controller location`);
+    }
+    public get(index: number): MenuController {
+        return this.controllers.get(index);
+    }
+
+    
+    public add(menuController: MenuController): void;
+    public add(menuController: MenuController, index: number): void;
+    public add(menuController: MenuController, index?: number): void {
+        if (index === undefined) {
+            this.menuView.add(menuController.menuView);
+            this.dataModel.add(menuController.dataModel);
+        } else {
+            this.menuView.add(menuController.menuView, index);
+            this.dataModel.add(menuController.dataModel, index);
+        }
+
+    }
+
+    public remove(menuController: MenuController): void;
+    public remove(index: number): void;
+    public remove(predicate: MenuController | number): void {
+        if (typeof predicate === 'number') {
+            this.menuView.remove(predicate);
+            this.dataModel.remove(predicate);
+        } else {
+            this.menuView.remove(predicate.menuView);
+            this.dataModel.remove(predicate.dataModel);
+        }
     }
     
     private addMouseDownDraggingEvent() {
