0f9745dda149abf181456064d397758436dcea31
Eli Simkin <es340716@g.risd.org>
2021-01-05 13:28:22 -0600

    Added more channels for the modification of files

--- src/ipc/channels.ts
+++ src/ipc/channels.ts
@@ -1,3 +1,5 @@
 export namespace IPCChannel {
     export const CONTEXT_MENU_NAV_OPEN = 'CONTEXT_MENU_NAV_OPEN';
+    export const DELETE_SELECTED_FILE = 'DELETE_SELECTED_FILE';
+    export const RENAME_SELECTED_FILE = 'RENAME_SELECTED_FILE';
 }
