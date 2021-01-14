52333be2d21dda7ac05b1b10ed4f96564201969c
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-14 10:37:59 -0600

    pruned

--- src/ipc/channels.ts
+++
@@ -1,6 +0,0 @@
-export namespace IPCChannel {
-    export const CONTEXT_MENU_NAV_OPEN = 'CONTEXT_MENU_NAV_OPEN';
-    export const DELETE_SELECTED_FILE = 'DELETE_SELECTED_FILE';
-    export const RENAME_SELECTED_FILE = 'RENAME_SELECTED_FILE';
-    export const ADD_NEW_FILE = 'ADD_NEW_FILE';
-}
