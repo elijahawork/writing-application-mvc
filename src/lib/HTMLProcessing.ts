339520d8760180e469f3793c6ee509ea2a62e5d0
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-13 21:04:52 -0600

    New architecture

--- src/lib/HTMLProcessing.ts
+++
@@ -1,8 +0,0 @@
-import { Coordinate } from "../types/Coordinate";
-
-export namespace HTMLProcessing {
-    export function getCoordinatesOfElementCenter(el: HTMLElement): Coordinate {
-        const box = el.getBoundingClientRect();
-        return { x: box.left + box.width / 2, y: box.top + box.height / 2 };
-    }
-}
