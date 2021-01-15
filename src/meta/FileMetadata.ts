af92bc0a2b77acd3ad649b9ae3710db2348fa29e
Eli Simkin <es340716@g.risd.org>
2021-01-15 17:43:51 -0600

    Matched Metadata purpose more to its job

--- src/meta/FileMetadata.ts
+++ src/meta/FileMetadata.ts
@@ -1,78 +1,21 @@
-import { Serializable } from "../interfaces/Serializable";
 import { HeightMap } from "../maps/HeightMap";
 import { Metadata } from "./Metadata";
 
 export class FileMetadata extends Metadata {
-    _label: string;
-    _parentId: number;
-    _position: number;
-    _conceptIds: Set<number>;
-    _templateId: number;
-    _timeline: HeightMap;
+    public label: string;
+    public parentId: number;
+    public position: number;
+    public conceptIds: Set<number>;
+    public templateId: number;
+    public timeline: HeightMap;
 
-    constructor(id: number, goalWords: number, notes: string, label: string, parentID: number, position: number, conceptIds: number[], templateId: number, timeline: HeightMap) {
+    constructor(id: number, goalWords: number, notes: string, label: string, parentID: number, position: number, conceptIds: Set<number>, templateId: number, timeline: HeightMap) {
         super(id, goalWords, notes);
-        this._label = label;
-        this._parentId = parentID;
-        this._position = position;
-        this._conceptIds = new Set(conceptIds);
-        this._templateId = templateId;
-        this._timeline = timeline;
-        this.writeToDisk();
-    }
-
-    public set label(v: string) {
-        this._label = v;
-        this.writeToDisk();
-    }
-    public set parentId(v: number) {
-        this._parentId = v;
-        this.writeToDisk();
-    }
-    public set position(v: number) {
-        this._position = v;
-        this.writeToDisk();
-    }
-    public set templateId(v: number) {
-        this._templateId = v;
-        this.writeToDisk();
-    }    
-    
-    public get label() {
-        return this._label;
-    }    
-    public get parentId() {
-        return this._parentId;
-    }
-    public get position() {
-        return this._position;
-    }
-    public get templateId() {
-        return this._templateId;
-    }
-    private get conceptIds(): number[] {
-        return [...this._conceptIds];
-    }
-
-    public joinConcept(concept: Metadata) {
-        this.conceptIds.push(concept.id);
-        this.writeToDisk();
-    }
-    public leaveConcept(concept: Metadata) {
-        this._conceptIds.delete(concept.id);
-        this.writeToDisk();
-    }
-    public joinTimeline(timeline: Metadata, height: number) {
-        this._timeline.set(timeline.id, height);
-        this.writeToDisk();
-    }
-    public leaveTimeline(timeline: Metadata) {
-        this._timeline.delete(timeline.id);
-        this.writeToDisk();
-    }
-
-    public serialize() {
-        const { id, goalWords, notes, label, parentId, position, conceptIds, templateId, _timeline } = this;
-        return JSON.stringify({ id, goalWords, notes, label, parentId, position, conceptIds, templateId, timeline: _timeline.serialize() });
+        this.label = label;
+        this.parentId = parentID;
+        this.position = position;
+        this.conceptIds = conceptIds;
+        this.templateId = templateId;
+        this.timeline = timeline;
     }
 }
--- src/meta/Metadata.ts
+++ src/meta/Metadata.ts
@@ -1,77 +1,11 @@
-import { __PROJ_NAME } from "..";
-import { join } from 'path';
-import fs from 'fs';
-import { Serializable } from "../interfaces/Serializable";
-
-const idMap: Map<number, Metadata> = new Map<number, Metadata>();
-
-export class Metadata implements Serializable {
-    private _id: number;
-    private _goalWords: number;
-    private _notes: string;
-    public static getMetadataById(id: number) {
-        return idMap.get(id);
-    }
-    public set id(v: number) {
-        this._id = v;
-        this.writeToDisk();
-    }    
-    public set goalWords(v : number) {
-        this._goalWords = v;
-        this.writeToDisk();
-    }
-    public set notes(v : string) {
-        this._notes = v;
-        this.writeToDisk();
-    }
-    public get notes() {
-        return this._notes;
-    }    
-    public get id() {
-        return this._id;
-    }
-    public get goalWords() {
-        return this._goalWords;
-    }
-
-    get filePath() {
-        return join(__PROJ_NAME, this.id.toString());
-    }
+export class Metadata {
+    public id: number;
+    public goalWords: number;
+    public  notes: string;
 
     constructor(id: number, goalWords: number, notes: string) {
-        this._id = id;
-        this._goalWords = goalWords;
-        this._notes = notes;
-        this.memoize();
-        this.writeToDisk();
-    }
-    
-    private memoize() {
-        idMap.set(this.id, this);
-    }
-
-    protected writeToDisk() {
-        fs.writeFileSync(this.filePath, this.serialize());
-    }
-    public static deserialize(serializedString: string): Metadata {
-        return JSON.parse(serializedString) as Metadata;
-    }
-    public static deserializeFromDisk(path: string) {
-        return Metadata.deserialize(Metadata.readFromDisk(path));
-    }
-    
-    private static readFromDisk(path: string): string {
-        return fs.readFileSync(path, 'utf-8');
-    }
-
-    public serialize() {
-        const { id, goalWords, notes } = this;
-        return JSON.stringify(
-            {
-                id,
-                goalWords,
-                notes
-            }
-        );
-    }
+        this.id = id;
+        this.goalWords = goalWords;
+        this.notes = notes;
+    }    
 }
