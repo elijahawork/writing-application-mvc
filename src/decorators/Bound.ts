d34864d1d15c93dc3baa11b97e5e2016397ffcff
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-28 09:26:25 -0600

    Decorator has unexpected behavior. Removing.

--- src/decorators/Bound.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Binds a function's "this" context to its containing class
- */
-export default function Bound(constr: any, key: string, info: PropertyDescriptor) {
-    info.value = (info.value as Function).bind(constr);
-}
