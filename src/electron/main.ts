c357904557de900af954588f77e6cf7fd695ad4e
Eli Simkin <es340716@g.risd.org>
2021-03-03 09:56:48 -0600

    Formatting

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -6,33 +6,29 @@ import * as tsElectronSourceMap from 'source-map-support';
 tsElectronSourceMap.install();
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        icon: join(__dirname, '..', '..', 'assets', 'icon.ico'),
-        title: 'Aesop Writing Software'
-    });
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: join(__dirname, '..', '..', 'assets', 'icon.ico'),
+    title: 'Aesop Writing Software',
+  });
 
-    console.log(join(__dirname, '..', 'assets', 'icon.ico'));
-    
-        
-    win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
-
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
