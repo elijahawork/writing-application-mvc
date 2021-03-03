d9cf9e800f23d1b28dda095e81d9927c69c0591c
Eli Simkin <es340716@g.risd.org>
2021-03-03 10:12:51 -0600

    Added a method to generate a unique identifier

--- src/models/AbstractModel.ts
+++ src/models/AbstractModel.ts
@@ -4,6 +4,8 @@ import { __PROJ_NAME } from '..';
 import SchemaField from '../decorators/SchemaField';
 import ISchema from '../schema/IModelSchema';
 
+const modelRegistry = new Map<number, AbstractModel<any>>();
+
 abstract class AbstractModel<Schema extends ISchema> {
   private readonly model: Schema = {} as Schema;
   private readonly EXT: string;
@@ -22,10 +24,18 @@ abstract class AbstractModel<Schema extends ISchema> {
   constructor(ext: string, obj: Schema) {
     this.EXT = ext;
     this.id = obj.id;
+    modelRegistry.set(this.id, this);
   }
 
   protected updateFile() {
     writeFileSync(this.filePath, JSON.stringify(this.model));
+  }
+  public static generateUniqueID(): number {
+    let id: number;
+    do {
+      id = Math.floor(Math.random() * modelRegistry.size);
+    } while (modelRegistry.has(id));
+    return id;
   }
 }
 
