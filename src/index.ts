f74342b09fc0a84558fd7988acd424ac312e81d8
elijahawork <64611104+elijahawork@users.noreply.github.com>
2021-03-13 21:21:15 -0600

    Set up a basic system to cehck if the project directory exists, and if it doesn't, it creates one

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { access, promises } from 'fs';
+import { join } from 'path';
+
+const __PROJ_NAME = join(__dirname, '..', 'protected');
+
+(async () => {
+  // creates the project directory
+  // if the project directory does not exist
+
+  // this works by attempting to access the directory
+  // if the directory does not exists, it throws an error
+  // and if there is an error, than that is truthy
+  // and that means that because no directory exists
+  // it can be made safely
+  // and then 
+  access(__PROJ_NAME, (pathDoesNotExist) => {
+    if (pathDoesNotExist) {
+      promises.mkdir(__PROJ_NAME);
+    }
+  });
+})();
